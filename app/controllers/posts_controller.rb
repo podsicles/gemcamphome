9e97609f090d467ff9e62db2b4374e933143d02b
podsicles <jessicaflores.business@gmail.com>
2023-07-05 15:32:36 +0800

    add method new and create

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,10 @@ class PostsController < ApplicationController
     def index
      @posts = Post.all
     end
-  end
+    def create
+      post = Post.new(params[:post].permit(:title, :content))
+      if post.save
+        redirect_to posts_path
+      end
+    end
+end
